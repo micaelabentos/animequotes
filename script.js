0882ba23f4d30b35b1c2c3591801b7d8e336f1e0
micaelabentos <bentosmicaela@gmail.com>
2022-02-17 23:37:22 -0300

    Add styles, sort list.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 async function start() {
-  const response = await fetch("https://animechan.vercel.app/api/available/anime");
+  const response = await fetch(
+    "https://animechan.vercel.app/api/available/anime"
+  );
   const data = await response.json();
   createAnimeList(data);
 }
@@ -7,35 +9,45 @@ async function start() {
 start();
 
 function createAnimeList(animeList) {
-
   document.getElementById("anime").innerHTML = `
     <select class="btn" onchange="loadByAnime(this.value)">
-
     <option>Select your favourite anime</option>
-    ${animeList.map( function (anime) {
-     return `<option>${anime}</option>`
-    })}
+    ${animeList
+      .map(function (anime) {
+        return `<option>${anime}</option>`;
+      })
+      .sort()
+      .splice(7)}
  
   </select>
     `;
 }
 
-async function loadByAnime(anime){
-    if (anime != "Select your favourite anime") {
-        const response = await fetch(`https://animechan.vercel.app/api/quotes/anime?title=${anime}`);
-        const data = await response.json();
-        console.log(data)
-        showQuotes(data);
-    }
-}
-
 function showQuotes(quotes) {
-  quotes.splice(0, 1);
   document.getElementById("quotes").innerHTML = 
   quotes.map( function(quotes)  {
-    return   `<p><span class="text-name">${quotes.character}: </span>
+      return `<p> <span class="text-name">${quotes.character}: </span>
     <span class="text-quote">"${quotes.quote}"</span>
-    </p>` 
-  }).join(" ")
- 
+    <a> <i class="fa-brands fa-twitter" id="twitter"></i> </a>
+             </p>`  
+             
+}).join(" ")
+
+document.getElementById("twitter").addEventListener("click", () => {
+  alert("Coming Soon");
+})
+
 }
+
+
+
+async function loadByAnime(anime) {
+  if (anime != "Select your favourite anime") {
+    const response = await fetch(
+      `https://animechan.vercel.app/api/quotes/anime?title=${anime}`
+    );
+    const data = await response.json();
+    showQuotes(data);
+  }
+}
+
